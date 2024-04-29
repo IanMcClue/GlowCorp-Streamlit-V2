ce60fd304c0d5dadf2882daecf781b764bd1af79
Ian McClue <123007216+IanMcClue@users.noreply.github.com>
2024-04-29 12:23:31 +0100

    Update streamlit_app.py
    squashing more bugs - FLL

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,106 +1,70 @@
+import streamlit as st
 import requests
 import zipfile
 import io
-from utils import icon
 import base64
 from streamlit_image_select import image_select
 from typing import Any
 
 # UI configurations
-st.set_page_config(page_title="Replicate Image Generator",
-                   page_icon=":bridge_at_night:",
+st.set_page_config(page_title="GlowCorp Character Factory",
+                   page_icon=":sparkle:",
                    layout="wide")
-icon.show_icon(":foggy:")
-st.markdown("# :rainbow[Text-to-Image Artistry Studio]")
 st.markdown("# :rainbow[GlowCorp Character Factory]🏭")
 
 # API Tokens and endpoints from `.streamlit/secrets.toml` file
 REPLICATE_API_TOKEN = st.secrets["REPLICATE_API_TOKEN"]
-REPLICATE_MODEL_ENDPOINTSTABILITY = st.secrets["REPLICATE_MODEL_ENDPOINTSTABILITY"]
-
-# Resources text, link, and logo
-replicate_text = "Stability AI SDXL Model on Replicate"
-replicate_link = "https://replicate.com/stability-ai/sdxl"
-replicate_logo = "https://storage.googleapis.com/llama2_release/Screen%20Shot%202023-07-21%20at%2012.34.05%20PM.png"
 REPLICATE_MODEL_FACE = st.secrets["REPLICATE_MODEL_FACE"]
 
 # Placeholders for images and gallery
 generated_images_placeholder = st.empty()
 gallery_placeholder = st.empty()
 
+
 # Predefined input data for the model
 PREDEFINED_INPUT = {
     "prompt": "Low poly pixelated of close-up of uploaded image, ps2 playstation psx gamecube game gta head 3d --style ddCHhSumaNyOrL1Q",
     "instant_id_strength": 0.8,
-    "neg_prompt": "boring"  # Corrected indentation here
+    "neg_prompt": "the worst image"  # Corrected indentation here
 }
 
 def configure_sidebar() -> None:
     """
     Setup and display the sidebar elements.
-    This function configures the sidebar of the Streamlit application, 
+
+    This function configures the sidebar of the Streamlit application,
     including the form for user inputs and the resources section.
     """
     with st.sidebar:
         with st.form("my_form"):
             st.info("**Yo fam! Start here ↓**", icon="👋🏾")
-            with st.expander(":rainbow[**Refine your output here**]"):
-                with st.expander(":rainbow[**Create your character**]"):
-                    # Advanced Settings (for the curious minds!)
-                    width = st.number_input("Width of output image", value=1024)
-                    height = st.number_input("Height of output image", value=1024)
-                    num_outputs = st.slider(
-                        "Number of images to output", value=1, min_value=1, max_value=4)
-                    scheduler = st.selectbox('Scheduler', ('DDIM', 'DPMSolverMultistep', 'HeunDiscrete',
-                                                           'KarrasDPM', 'K_EULER_ANCESTRAL', 'K_EULER', 'PNDM'))
-                    num_inference_steps = st.slider(
-                        "Number of denoising steps", value=50, min_value=1, max_value=500)
-                    guidance_scale = st.slider(
-                        "Scale for classifier-free guidance", value=7.5, min_value=1.0, max_value=50.0, step=0.1)
-                    prompt_strength = st.slider(
-                        "Prompt strength when using img2img/inpaint(1.0 corresponds to full destruction of infomation in image)", value=0.8, max_value=1.0, step=0.1)
-                    refine = st.selectbox(
-                        "Select refine style to use (left out the other 2)", ("expert_ensemble_refiner", "None"))
-                    high_noise_frac = st.slider(
-                        "Fraction of noise to use for `expert_ensemble_refiner`", value=0.8, max_value=1.0, step=0.1)
-                prompt = st.text_area(
-                    ":orange[**Enter prompt: start typing, Shakespeare ✍🏾**]",
-                    value="An astronaut riding a rainbow unicorn, cinematic, dramatic")
-                negative_prompt = st.text_area(":orange[**Party poopers you don't want in image? 🙅🏽‍♂️**]",
-                                               value="the absolute worst quality, distorted features",
-                                               help="This is a negative prompt, basically type what you don't want to see in the generated image")
+            with st.expander(":rainbow[**Create your character**]"):
+                # Advanced Settings (for the curious minds!)
                 style = st.selectbox('Choose an image style', ("PS2", "Pixelated", "Clay", "Emoji", "3D"))
                 uploaded_image = st.file_uploader("Upload an image file", type=["jpg", "jpeg", "png", "webp"])
 
             # The Big Red "Submit" Button!
             submitted = st.form_submit_button(
-                "Submit", type="primary", use_container_width=True)
+                "Submit", use_container_width=True)
 
-        # Credits and resources
-        st.divider()
-        st.markdown(
-            ":orange[**Resources:**]  \n"
-            f"<img src='{replicate_logo}' style='height: 1em'> [{replicate_text}]({replicate_link})",
-            unsafe_allow_html=True
-        )
+            # Credits and resources
         st.markdown(
             """
             ---
-            Follow me on:
             Follow 🚺 Curetique(owners of GlowCorp✨) on:
-            𝕏 → [@tonykipkemboi](https://twitter.com/tonykipkemboi)
-            LinkedIn → [Tony Kipkemboi](https://www.linkedin.com/in/tonykipkemboi)
+
             Tiktok → [@curetique](https://www.tiktok.com/@curetique)
+
             """
         )
 
-        return submitted, width, height, num_outputs, scheduler, num_inference_steps, guidance_scale, prompt_strength, refine, high_noise_frac, prompt, negative_prompt
+        return submitted, style, uploaded_image
 
 def main_page(submitted: bool, style: str, uploaded_image: Any) -> None:
     """Main page layout and logic for generating images.
+
     Args:
         submitted (bool): Flag indicating whether the form has been submitted.
-        style (str): Chosen image style.
         uploaded_image (Any): The file-like object of the selected image.
     """
     if style == "PS2":
@@ -111,86 +75,81 @@ def main_page(submitted: bool, style: str, uploaded_image: Any) -> None:
     if submitted:
         with st.status('👩🏾‍🍳 Whipping up your character...', expanded=True) as status:
             st.write("⚙️ Model initiated")
-            st.write("🙆‍♀️ Stand up and stretch in the meantime")
+            st.write("🙆‍♀️ Stand up and strecth in the meantime")
             try:
                 # Only call the API if the "Submit" button was pressed
-                if uploaded_image is not None:
+                if submitted:
                     # Convert the uploaded image to the required format
-                    image_data = uploaded_image.read()
-                    image_uri = f"data:image/jpeg;base64,{base64.b64encode(image_data).decode()}"
-                else:
-                    st.error("Please select an image file.", icon="🚨")
-                    return
+                    if uploaded_image is not None:
+                        image_data = uploaded_image.read()
+                        image_uri = f"data:image/jpeg;base64,{base64.b64encode(image_data).decode()}"
+                    else:
+                        st.error("Please select an image file.", icon="🚨")
+                        return
 
-                # Calling the replicate API to get the image
-                with generated_images_placeholder.container():
-                    all_images = []  # List to store all generated images
-                    output = replicate.run(
-                        REPLICATE_MODEL_ENDPOINTSTABILITY,
-                        REPLICATE_MODEL_FACE,
-                        input={
-                            "prompt": prompt,
-                            "width": width,
-                            "height": height,
-                            "num_outputs": num_outputs,
-                            "scheduler": scheduler,
-                            "num_inference_steps": num_inference_steps,
-                            "guidance_scale": guidance_scale,
-                            "prompt_strength": prompt_strength,
-                            "refine": refine,
-                            "high_noise_frac": high_noise_frac,
-                            "image": image_uri,
-                            "style": style,
-                            "prompt": PREDEFINED_INPUT["prompt"],
-                            "neg_prompt": PREDEFINED_INPUT["neg_prompt"],
-                            "instant_id_strength": PREDEFINED_INPUT["instant_id_strength"]
-                        }
-                    )
-                    if output:
-                        st.toast(
-                            'Your character has been generated!', icon='😍')
-                        # Save generated image to session state
-                        st.session_state.generated_image = output
+                    # Calling the replicate API to get the image
+                    with generated_images_placeholder.container():
+                        all_images = []  # List to store all generated images
+                        output = replicate.run(
+                            REPLICATE_MODEL_FACE,
+                            input={
+                                "image": image_uri,
+                                "style": style,
+                                "prompt": PREDEFINED_INPUT["prompt"],
+                                "neg_prompt": PREDEFINED_INPUT["neg_prompt"],
+                                "instant_id_strength": PREDEFINED_INPUT["instant_id_strength"]
+                            }
+                        )
+                        if output:
+                            st.toast(
+                                'Your character has been generated!', icon='😍')
+                            # Save generated image to session state
+                            st.session_state.generated_image = output
 
-                        for i, image in enumerate(output['images']):
-                            # Add image to the list
-                            all_images.append(image)
+                            # Displaying the image
+                            for image in st.session_state.generated_image:
+                                with st.container():
+                                    st.image(image, caption="Generated Image 🎈",
+                                             use_column_width=True)
+                                    # Add image to the list
+                                    all_images.append(image)
 
-                        # Save all generated images to session state
-                        st.session_state.all_images = all_images
+                            # Save all generated images to session state
+                            st.session_state.all_images = all_images
 
-                        # Create a BytesIO object
-                        zip_io = io.BytesIO()
+                            # Create a BytesIO object
+                            zip_io = io.BytesIO()
 
-                        # Download option for each image
-                        with zipfile.ZipFile(zip_io, 'w') as zipf:
-                            for i, image in enumerate(st.session_state.all_images):
-                                response = requests.get(image)
-                                if response.status_code == 200:
-                                    image_data = response.content
-                                    # Write each image to the zip file with a name
-                                    zipf.writestr(
-                                        f"output_file_{i+1}.png", image_data)
-                                else:
-                                    st.error(
-                                        f"Failed to fetch image {i+1} from {image}. Error code: {response.status_code}", icon="🚨")
-                        # Create a download button for the zip file
-                        st.download_button(
-                            ":red[**Download All Images**]", data=zip_io.getvalue(), file_name="output_files.zip", mime="application/zip", use_container_width=True)
-                status.update(label="✅ Images generated!",
-                              state="complete", expanded=False)
+                            # Download option for each image
+                            with zipfile.ZipFile(zip_io, 'w') as zipf:
+                                for i, image in enumerate(st.session_state.all_images):
+                                    response = requests.get(image)
+                                    if response.status_code == 200:
+                                        image_data = response.content
+                                        # Write each image to the zip file with a name
+                                        zipf.writestr(
+                                            f"output_file_{i+1}.png", image_data)
+                                    else:
+                                        st.error(
+                                            f"Failed to fetch image {i+1} from {image}. Error code: {response.status_code}", icon="🚨")
+                            # Create a download button for the zip file
+                            st.download_button(
+                                ":red[**Download All Images**]", data=zip_io.getvalue(), file_name="output_files.zip", mime="application/zip", use_container_width=True)
+                    status.update(label="✅ Images generated!",
+                                  state="complete", expanded=False)
             except Exception as e:
                 print(e)
                 st.error(f'Encountered an error: {e}', icon="🚨")
 
+    # If not submitted, chill here 🍹
     else:
         pass
 
     # Footer
     st.divider()
     footer = """<div style="text-align: center;">
-                <a href="https://visitorbadge.io/status?path=https%3A%2F%2FGlowCorp.streamlit.app%2F">
-                    <img src="https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2FGlowCorp.streamlit.app%2F&label=GlowCorp&labelColor=%23ffffff&countColor=%23000000&style=plastic" />
+                <a href="https://visitorbadge.io/status?path=https%3A%2F%2Faurascope.streamlit.app%2F">
+                    <img src="https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Faurascope.streamlit.app%2F&label=aurascope&labelColor=%23ffffff&countColor=%23000000&style=plastic" />
                 </a>
             </div>"""
     st.markdown(footer, unsafe_allow_html=True)
@@ -198,20 +157,8 @@ def main_page(submitted: bool, style: str, uploaded_image: Any) -> None:
     # Gallery display for inspo
     with gallery_placeholder.container():
         img = image_select(
-            label="Like what you see? Right-click and save! It's not stealing if we're sharing! 😉",
             label="(Click on the sidebar to get started! 😉)",
             images=[
-                "gallery/farmer_sunset.png", "gallery/astro_on_unicorn.png",
-                "gallery/friends.png", "gallery/wizard.png", "gallery/puppy.png",
-                "gallery/cheetah.png", "gallery/viking.png",
-            ],
-            captions=["A farmer tilling a farm with a tractor during sunset, cinematic, dramatic",
-                      "An astronaut riding a rainbow unicorn, cinematic, dramatic",
-                      "A group of friends laughing and dancing at a music festival, joyful atmosphere, 35mm film photography",
-                      "A wizard casting a spell, intense magical energy glowing from his hands, extremely detailed fantasy illustration",
-                      "A cute puppy playing in a field of flowers, shallow depth of field, Canon photography",
-                      "A cheetah mother nurses her cubs in the tall grass of the Serengeti. The early morning sun beams down through the grass. National Geographic photography by Frans Lanting",
-                      "A close-up portrait of a bearded viking warrior in a horned helmet. He stares intensely into the distance while holding a battle axe. Dramatic mood lighting, digital oil painting",
                  "gallery/before.jpg", "gallery/after.jpg", ],
             captions=["before: unedited picture",
                       "after: new character unlocked",
@@ -219,10 +166,11 @@ def main_page(submitted: bool, style: str, uploaded_image: Any) -> None:
             use_container_width=True
         )
 
-
 def main():
     """
     Main function to run the Streamlit application.
+
+    This function initializes the sidebar configuration and the main page layout.
     It retrieves the user inputs from the sidebar, and passes them to the main page function.
     The main page function then generates images based on these inputs.
     """
